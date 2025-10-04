a4ad1be0927ec9a74291a00d915fc9316fde4693
alexpasek <webtoronto22@gmail.com>
2025-10-04 00:20:12 -0400

    ER

--- functions/_worker.js
+++ functions/_worker.js
@@ -2,7 +2,7 @@
 import worker from "../.open-next/worker.js";
 
 export const config = {
-    compatibility_date: "2024-11-08",
+    compatibility_date: "2025-09-01",
     compatibility_flags: ["nodejs_compat"],
 };
 
