4fda1c9b5981cde21fd619a1d27559eb5bc33981
alexpasek <webtoronto22@gmail.com>
2025-10-04 01:18:43 -0400

    pages: add explicit onRequest handler calling OpenNext worker

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,3 +1,8 @@
 // functions/[[path]].js
-export { default }
-from "../.open-next/worker.js";
+import worker from "../.open-next/worker.js";
+
+// Service-Worker style handler that Wrangler recognizes
+export async function onRequest(context) {
+    // context = { request, env, params, next, data, waitUntil }
+    return worker.fetch(context.request, context.env, context);
+}
